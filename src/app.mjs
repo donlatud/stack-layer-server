76182e14af50f975a82d892e6b9081f98d72dad1
donlatud <donlatud@gmail.com>
2026-02-02 18:25:58 +0700

    Add Vercel configuration and enable CORS in Express app

--- src/app.mjs
+++ src/app.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(express.json());
 
 // GET /profiles - ดูข้อมูลโปรไฟล์ของ John
